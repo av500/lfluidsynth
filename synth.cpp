9111d03fdb4e9864c9e729153d08f64faf2100b7
Vladimir Pantelic <vladimir@pantelic.net>
2023-09-22 12:31:57 +0200

    reformat and drop cpp wrapper, we call the fluid_synth API directly
    Signed-off-by: Vladimir Pantelic <vladimir@pantelic.net>

--- synth.cpp
+++ synth.cpp
@@ -6,96 +6,124 @@
 #include <stdint.h>
 #include <vector>
 
-#include "LightFluidSynth.hpp"
+#include "lfluidsynth.h"
 
-#include "RtAudio.h"
+#include "rtaudio/RtAudio.h"
 
-//#define AUDIO_FORMAT RTAUDIO_FLOAT32
-#define AUDIO_FORMAT RTAUDIO_SINT16
+//#define AUDIO_FORMAT 	RTAUDIO_FLOAT32
+#define AUDIO_FORMAT 	RTAUDIO_SINT16
 
-#define FRAME_SIZE 128
+#define POLYPHONY 	64
+#define SAMPLE_RATE 	44100
+
+#define FRAME_SIZE 	32
 
 using namespace std;
 
+int audioCallback(void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames, double streamTime, RtAudioStreamStatus status, void *userData)
 {
+	fluid_synth_t *synth = (fluid_synth_t*)userData;
+
+	switch (AUDIO_FORMAT) {
+	case RTAUDIO_FLOAT32:
+		{
+			float *buf = (float*)outputBuffer;
+			fluid_synth_write_float(synth, nBufferFrames, buf, 0, 2, buf, 1, 2);
+
+		}
+		break;
+	case RTAUDIO_SINT16:
+		{
+			int16_t *buf = (int16_t*)outputBuffer;
+			fluid_synth_write_s16(synth, nBufferFrames, (int16_t *)buf, 0, 2, (int16_t *)buf, 1, 2);
+
+		}
+		break;
+	case RTAUDIO_SINT32:
+		{
+			int32_t *buf = (int32_t*)outputBuffer;
+			fluid_synth_write_s32(synth, nBufferFrames, (int32_t *)buf, 0, 2, (int32_t *)buf, 1, 2);
+
+		}
+		break;
+	}
+
+	return 0;
 }
 
-int audioCallback( void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
-                   double streamTime, RtAudioStreamStatus status, void *userData )
+int main(int argc, char **argv)
 {
-  LightFluidSynth *synth = (LightFluidSynth *)userData;
+/*
+	if (argc != 2) {
+    		printf("Usage: synth file.sf2\n");
+    		exit(0);
+	}
+*/
+	fluid_synth_t *synth;
 
-  switch (AUDIO_FORMAT) {
-  case RTAUDIO_FLOAT32:
-  {
-    float *buf = (float *)outputBuffer;
-    synth->writeStereoFloat(buf, nBufferFrames);
+	fluid_settings_t settings;
+	fluid_synth_settings(&settings);
+	settings.sample_rate = SAMPLE_RATE;
+	settings.reverb      = 0;
+	settings.chorus      = 0;
+	settings.polyphony   = POLYPHONY;
+	settings.verbose     = 0;
 
-  }
-  break;
-  case RTAUDIO_SINT16:
-  {
-    int16_t *buf = (int16_t *)outputBuffer;
-    synth->writeStereoS16((short *)buf, nBufferFrames);
-  }
-  break;
-  case RTAUDIO_SINT32:
-  {
-    int32_t *buf = (int32_t *)outputBuffer;
-    synth->writeStereoS32((int *)buf, nBufferFrames);
+	synth = new_fluid_synth(&settings);
 
-  }
-  break;
-  }
+//	fluid_synth_set_interp_method(synth, -1, FLUID_INTERP_LINEAR);
+	fluid_synth_set_interp_method(synth, -1, FLUID_INTERP_NONE);
 
-  return 0;
-}
+//	usynth->loadSF2(argv[1]);
+	int sfont_id = fluid_synth_sfload(synth, (char *)"FluidR3 GM.sf2", 1);
 
-int main(int argc, char** argv)
-{
+	RtAudio dac;
+	RtAudio::StreamParameters rtParams;
 
-  if (argc != 2) {
-    printf("Usage: synth file.sf2\n");
-    exit(0);
-  }
+	// Determine the number of devices available
+	unsigned int devices = dac.getDeviceCount();
+	// Scan through devices for various capabilities
+	RtAudio::DeviceInfo info;
+	for (unsigned int i = 0; i < devices; i++) {
+		info = dac.getDeviceInfo(i);
+		if (info.probed == true) {
+			std::cout << "device " << " = " << info.name;
+			std::cout << ": maximum output channels = " << info.outputChannels << "\n";
+		}
+	}
 
-  LightFluidSynth *usynth;
+	rtParams.deviceId = dac.getDefaultOutputDevice();
+	rtParams.nChannels = 2;
+	unsigned int bufferFrames = FRAME_SIZE;
 
-  usynth = new LightFluidSynth();
+	RtAudio::StreamOptions options;
+	options.flags = RTAUDIO_SCHEDULE_REALTIME;
 
-  usynth->loadSF2(argv[1]);
-//  usynth->loadSF2("tim.sf2");
+	dac.openStream(&rtParams, NULL, AUDIO_FORMAT, SAMPLE_RATE, &bufferFrames, &audioCallback, (void *)synth, &options);
+	dac.startStream();
 
-//   RtAudio dac(RtAudio::LINUX_PULSE);
-  RtAudio dac;
-  RtAudio::StreamParameters rtParams;
+	int note = 0;
+	int chan = 0;
+	int prog = 1;
+	while (1) {
+		fluid_synth_program_change(synth, chan, prog);
+		int n = 40 + (note++ % 16);
+		printf("prog %3d  note %3d\n", prog, n);
 
-  // Determine the number of devices available
-  unsigned int devices = dac.getDeviceCount();
-  // Scan through devices for various capabilities
-  RtAudio::DeviceInfo info;
-  for ( unsigned int i = 0; i < devices; i++ ) {
-    info = dac.getDeviceInfo( i );
-    if ( info.probed == true ) {
-      std::cout << "device " << " = " << info.name;
-      std::cout << ": maximum output channels = " << info.outputChannels << "\n";
-    }
-  }
-//  rtParams.deviceId = 3;
-  rtParams.deviceId = dac.getDefaultOutputDevice();
-  rtParams.nChannels = 2;
-  unsigned int bufferFrames = FRAME_SIZE;
+		fluid_synth_noteon(synth, chan, n, 127);
+		usleep(90000);
+		fluid_synth_noteoff(synth, chan, n);
+		usleep(50000);
 
-  RtAudio::StreamOptions options;
-  options.flags = RTAUDIO_SCHEDULE_REALTIME;
+		prog = (prog + 1) % 128;
+	}
 
-  dac.openStream( &rtParams, NULL, AUDIO_FORMAT, SAMPLE_RATE, &bufferFrames, &audioCallback, (void *)usynth, &options );
-  dac.startStream();
+	printf("\n\nPress Enter to stop\n\n");
+	cin.get();
+	dac.stopStream();
 
-  printf("\n\nPress Enter to stop\n\n");
-  cin.get();
-  dac.stopStream();
+	fluid_synth_sfunload(synth,sfont_id,1);
+    	delete_fluid_synth(synth);
 
-  delete(usynth);
-  return 0;
+	return 0;
 }
